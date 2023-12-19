df37e227e33eb5c6c05c63082cc8f1752a0fa271
Rodrigo Alves <rodrigoalvesbrasileiro@gmail.com>
2023-12-18 23:07:07 -0300

    feat: new components and adjustments in routes of app

---
+++ components/barbecue-card.tsx
@@ -0,0 +1,15 @@
+interface BarbecueCardProps {
+  id: string
+  title: string
+  description: string
+  date: string
+  location: string
+  image: string
+}
+
+export function BarbecueCard({  }: BarbecueCardProps) {
+
+  return (
+    
+  )
+}
